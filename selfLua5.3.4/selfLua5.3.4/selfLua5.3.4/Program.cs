2724fd591dba8c73b1319a1a30f4b6fc555f6ed2
wth <690879430@qq.com>
2019-08-22 15:50:07 +0800

    添加selflua工程

---
+++ selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace selfLua5._3._4
+//{
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("test");
+        Console.ReadLine();
+    }
+}
+//}
