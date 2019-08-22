d75e248ad898ce0d261ad69d4dfbbc343af155c3
wth <690879430@qq.com>
2019-08-22 17:34:03 +0800

    补提交

--- selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/Program.cs
+++ selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/Program.cs
@@ -22,21 +22,54 @@ public class BinaryChunk
     public double luacNum;
 }
 
+public class Prototype
+{
+    public string Source;
+    public UInt32 LineDefined;
+    public UInt32 LastLineDefined;
+    public byte NumParams;
+    public byte IsVararg;
+    public byte MaxStackSize;
+    public UInt32[] Code;
+    // constants;
+    // upvalue;
+    public Prototype[] Protos;
+    public UInt32[] LineInfo;
+    // LocVar
+    public string[] UpvalueNames;
+}
+
 class Program
 {
     static void ProcessData(byte[] data)
     {
         BinaryChunk chunk = new BinaryChunk();
         MemoryStream stream = new MemoryStream(data);
+        BinaryReader reader = new BinaryReader(stream);
         for(int i = 0; i < chunk.signature.Length; ++i)
         {
             chunk.signature[i] = (byte)stream.ReadByte();
         }
+        chunk.version = (byte)stream.ReadByte();
+        chunk.format = (byte)stream.ReadByte();
+        for(int i = 0; i < chunk.luacData.Length; ++i)
+        {
+            chunk.luacData[i] = (byte)stream.ReadByte();
+        }
+        chunk.cintSize = (byte)stream.ReadByte();
+        chunk.sizetSize = (byte)stream.ReadByte();
+        chunk.instructionSize = (byte)stream.ReadByte();
+        chunk.luaIntegerSize = (byte)stream.ReadByte();
+        chunk.luaNumberSize = (byte)stream.ReadByte();
+        chunk.luacInt = reader.ReadInt64();
+        chunk.luacNum = reader.ReadDouble();
+
+        int test = 0;
     }
 
     static void Main(string[] args)
     {
-        byte[] data = File.ReadAllBytes("");
+        byte[] data = File.ReadAllBytes(@"E:\gitHub\luaLearn\LuaLearn\originLua5.3.4\originLua5.3.4\Debug\luac.out");
         ProcessData(data);
 
         Console.WriteLine("test");
