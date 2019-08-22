7b875485b6b6095e21c3cea513aca31355f602ec
wth <690879430@qq.com>
2019-08-22 16:58:16 +0800

    补提交

--- selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/Program.cs
+++ selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/Program.cs
@@ -6,6 +6,21 @@ using System.Threading.Tasks;
 
 //namespace selfLua5._3._4
 //{
+public class BinaryChunk
+{
+    public byte[] signature = new byte[4];
+    public byte version;
+    public byte format;
+    public byte[] luacData = new byte[6];
+    public byte cintSize;
+    public byte sizetSize;
+    public byte instructionSize;
+    public byte luaIntegerSize;
+    public byte luaNumberSize;
+    public Int64 luacInt;
+    public double luacNum;
+}
+
 class Program
 {
     static void Main(string[] args)
