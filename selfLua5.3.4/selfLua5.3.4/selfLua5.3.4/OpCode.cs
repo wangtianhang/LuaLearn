be92ce3352f7d4d8a11d96263767db78c8ebedc9
wth <690879430@qq.com>
2019-08-22 23:42:25 +0800

    补提交

--- selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/OpCode.cs
+++ selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/OpCode.cs
@@ -65,53 +65,53 @@ public class Instruction
 public struct OpCode
 {
     /*       T  A    B       C     mode */
-    static OpCode MOVE = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iABC); // R(A) := R(B)
-    static OpCode LOADK = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgN, OpMode.iABx); // R(A) := Kst(Bx)
-    static OpCode LOADKX = new OpCode(0, 1, OpArgMask.OpArgN, OpArgMask.OpArgN, OpMode.iABx); // R(A) := Kst(extra arg)
-    static OpCode LOADBOOL = new OpCode(0, 1, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iABC); // R(A) := (bool)B; if (C) pc++
-    static OpCode LOADNIL = new OpCode(0, 1, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABC); // R(A), R(A+1), ..., R(A+B) := nil
-    static OpCode GETUPVAL = new OpCode(0, 1, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABC); // R(A) := UpValue[B]
-    static OpCode GETTABUP = new OpCode(0, 1, OpArgMask.OpArgU, OpArgMask.OpArgK, OpMode.iABC); // R(A) := UpValue[B][RK(C)]
-    static OpCode GETTABLE = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgK, OpMode.iABC); // R(A) := R(B)[RK(C)]
-    static OpCode SETTABUP = new OpCode(0, 0, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // UpValue[A][RK(B)] := RK(C)
-    static OpCode SETUPVAL = new OpCode(0, 0, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABC); // UpValue[B] := R(A)
-    static OpCode SETTABLE = new OpCode(0, 0, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A)[RK(B)] := RK(C)
-    static OpCode NEWTABLE = new OpCode(0, 1, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iABC); // R(A) := {} (size = B,C)
-    static OpCode SELF = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgK, OpMode.iABC); // R(A+1) := R(B); R(A) := R(B)[RK(C)]
-    static OpCode ADD = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) + RK(C)
-    static OpCode SUB = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) - RK(C)
-    static OpCode MUL = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) * RK(C)
-    static OpCode MOD = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) % RK(C)
-    static OpCode POW = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) ^ RK(C)
-    static OpCode DIV = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) / RK(C)
-    static OpCode IDIV = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) // RK(C)
-    static OpCode BAND = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) & RK(C)
-    static OpCode BOR = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) | RK(C)
-    static OpCode BXOR = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) ~ RK(C)
-    static OpCode SHL = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) << RK(C)
-    static OpCode SHR = new OpCode(0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) >> RK(C)
-    static OpCode UNM = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iABC); // R(A) := -R(B)
-    static OpCode BNOT = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iABC); // R(A) := ~R(B)
-    static OpCode NOT = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iABC); // R(A) := not R(B)
-    static OpCode LEN = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iABC); // R(A) := length of R(B)
-    static OpCode CONCAT = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgR, OpMode.iABC); // R(A) := R(B).. ... ..R(C)
-    static OpCode JMP = new OpCode(0, 0, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iAsBx); // pc+=sBx; if (A) close all upvalues >= R(A - 1)
-    static OpCode EQ = new OpCode(1, 0, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // if ((RK(B) == RK(C)) ~= A) then pc++
-    static OpCode LT = new OpCode(1, 0, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // if ((RK(B) <  RK(C)) ~= A) then pc++
-    static OpCode LE = new OpCode(1, 0, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // if ((RK(B) <= RK(C)) ~= A) then pc++
-    static OpCode TEST = new OpCode(1, 0, OpArgMask.OpArgN, OpArgMask.OpArgU, OpMode.iABC); // if not (R(A) <=> C) then pc++
-    static OpCode TESTSET = new OpCode(1, 1, OpArgMask.OpArgR, OpArgMask.OpArgU, OpMode.iABC); // if (R(B) <=> C) then R(A) := R(B) else pc++
-    static OpCode CALL = new OpCode(0, 1, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iABC); // R(A), ... ,R(A+C-2) := R(A)(R(A+1), ... ,R(A+B-1))
-    static OpCode TAILCALL = new OpCode(0, 1, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iABC); // return R(A)(R(A+1), ... ,R(A+B-1))
-    static OpCode RETURN = new OpCode(0, 0, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABC); // return R(A), ... ,R(A+B-2)
-    static OpCode FORLOOP = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iAsBx); // R(A)+=R(A+2); if R(A) <?= R(A+1) then { pc+=sBx; R(A+3)=R(A) }
-    static OpCode FORPREP = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iAsBx); // R(A)-=R(A+2); pc+=sBx
-    static OpCode TFORCALL = new OpCode(0, 0, OpArgMask.OpArgN, OpArgMask.OpArgU, OpMode.iABC); // R(A+3), ... ,R(A+2+C) := R(A)(R(A+1), R(A+2));
-    static OpCode TFORLOOP = new OpCode(0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iAsBx); // if R(A+1) ~= nil then { R(A)=R(A+1); pc += sBx }
-    static OpCode SETLIST = new OpCode(0, 0, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iABC); // R(A)[(C-1)*FPF+i] := R(A+i), 1 <= i <= B
-    static OpCode CLOSURE = new OpCode(0, 1, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABx); // R(A) := closure(KPROTO[Bx])
-    static OpCode VARARG = new OpCode(0, 1, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABC); // R(A), R(A+1), ..., R(A+B-2) = vararg
-    static OpCode EXTRAARG = new OpCode(0, 0, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iAx); // extra (larger) argument for previous opcode
+    static OpCode MOVE = new OpCode("MOVE", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iABC); // R(A) := R(B)
+    static OpCode LOADK = new OpCode("LOADK", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgN, OpMode.iABx); // R(A) := Kst(Bx)
+    static OpCode LOADKX = new OpCode("LOADKX", 0, 1, OpArgMask.OpArgN, OpArgMask.OpArgN, OpMode.iABx); // R(A) := Kst(extra arg)
+    static OpCode LOADBOOL = new OpCode("LOADBOOL", 0, 1, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iABC); // R(A) := (bool)B; if (C) pc++
+    static OpCode LOADNIL = new OpCode("LOADNIL", 0, 1, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABC); // R(A), R(A+1), ..., R(A+B) := nil
+    static OpCode GETUPVAL = new OpCode("GETUPVAL", 0, 1, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABC); // R(A) := UpValue[B]
+    static OpCode GETTABUP = new OpCode("GETTABUP", 0, 1, OpArgMask.OpArgU, OpArgMask.OpArgK, OpMode.iABC); // R(A) := UpValue[B][RK(C)]
+    static OpCode GETTABLE = new OpCode("GETTABLE", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgK, OpMode.iABC); // R(A) := R(B)[RK(C)]
+    static OpCode SETTABUP = new OpCode("SETTABUP", 0, 0, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // UpValue[A][RK(B)] := RK(C)
+    static OpCode SETUPVAL = new OpCode("SETUPVAL", 0, 0, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABC); // UpValue[B] := R(A)
+    static OpCode SETTABLE = new OpCode("SETTABLE", 0, 0, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A)[RK(B)] := RK(C)
+    static OpCode NEWTABLE = new OpCode("NEWTABLE", 0, 1, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iABC); // R(A) := {} (size = B,C)
+    static OpCode SELF = new OpCode("SELF", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgK, OpMode.iABC); // R(A+1) := R(B); R(A) := R(B)[RK(C)]
+    static OpCode ADD = new OpCode("ADD", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) + RK(C)
+    static OpCode SUB = new OpCode("SUB", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) - RK(C)
+    static OpCode MUL = new OpCode("MUL", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) * RK(C)
+    static OpCode MOD = new OpCode("MOD", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) % RK(C)
+    static OpCode POW = new OpCode("POW", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) ^ RK(C)
+    static OpCode DIV = new OpCode("DIV", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) / RK(C)
+    static OpCode IDIV = new OpCode("IDIV", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) // RK(C)
+    static OpCode BAND = new OpCode("BAND", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) & RK(C)
+    static OpCode BOR = new OpCode("BOR", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) | RK(C)
+    static OpCode BXOR = new OpCode("BXOR", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) ~ RK(C)
+    static OpCode SHL = new OpCode("SHL", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) << RK(C)
+    static OpCode SHR = new OpCode("SHR", 0, 1, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // R(A) := RK(B) >> RK(C)
+    static OpCode UNM = new OpCode("UNM", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iABC); // R(A) := -R(B)
+    static OpCode BNOT = new OpCode("BNOT", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iABC); // R(A) := ~R(B)
+    static OpCode NOT = new OpCode("NOT", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iABC); // R(A) := not R(B)
+    static OpCode LEN = new OpCode("LEN", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iABC); // R(A) := length of R(B)
+    static OpCode CONCAT = new OpCode("CONCAT", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgR, OpMode.iABC); // R(A) := R(B).. ... ..R(C)
+    static OpCode JMP = new OpCode("JMP", 0, 0, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iAsBx); // pc+=sBx; if (A) close all upvalues >= R(A - 1)
+    static OpCode EQ = new OpCode("EQ", 1, 0, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // if ((RK(B) == RK(C)) ~= A) then pc++
+    static OpCode LT = new OpCode("LT", 1, 0, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // if ((RK(B) <  RK(C)) ~= A) then pc++
+    static OpCode LE = new OpCode("LE", 1, 0, OpArgMask.OpArgK, OpArgMask.OpArgK, OpMode.iABC); // if ((RK(B) <= RK(C)) ~= A) then pc++
+    static OpCode TEST = new OpCode("TEST", 1, 0, OpArgMask.OpArgN, OpArgMask.OpArgU, OpMode.iABC); // if not (R(A) <=> C) then pc++
+    static OpCode TESTSET = new OpCode("TESTSET", 1, 1, OpArgMask.OpArgR, OpArgMask.OpArgU, OpMode.iABC); // if (R(B) <=> C) then R(A) := R(B) else pc++
+    static OpCode CALL = new OpCode("CALL", 0, 1, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iABC); // R(A), ... ,R(A+C-2) := R(A)(R(A+1), ... ,R(A+B-1))
+    static OpCode TAILCALL = new OpCode("TAILCALL", 0, 1, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iABC); // return R(A)(R(A+1), ... ,R(A+B-1))
+    static OpCode RETURN = new OpCode("RETURN", 0, 0, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABC); // return R(A), ... ,R(A+B-2)
+    static OpCode FORLOOP = new OpCode("FORLOOP", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iAsBx); // R(A)+=R(A+2); if R(A) <?= R(A+1) then { pc+=sBx; R(A+3)=R(A) }
+    static OpCode FORPREP = new OpCode("FORPREP", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iAsBx); // R(A)-=R(A+2); pc+=sBx
+    static OpCode TFORCALL = new OpCode("TFORCALL", 0, 0, OpArgMask.OpArgN, OpArgMask.OpArgU, OpMode.iABC); // R(A+3), ... ,R(A+2+C) := R(A)(R(A+1), R(A+2));
+    static OpCode TFORLOOP = new OpCode("TFORLOOP", 0, 1, OpArgMask.OpArgR, OpArgMask.OpArgN, OpMode.iAsBx); // if R(A+1) ~= nil then { R(A)=R(A+1); pc += sBx }
+    static OpCode SETLIST = new OpCode("SETLIST", 0, 0, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iABC); // R(A)[(C-1)*FPF+i] := R(A+i), 1 <= i <= B
+    static OpCode CLOSURE = new OpCode("CLOSURE", 0, 1, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABx); // R(A) := closure(KPROTO[Bx])
+    static OpCode VARARG = new OpCode("VARARG", 0, 1, OpArgMask.OpArgU, OpArgMask.OpArgN, OpMode.iABC); // R(A), R(A+1), ..., R(A+B-2) = vararg
+    static OpCode EXTRAARG = new OpCode("EXTRAARG", 0, 0, OpArgMask.OpArgU, OpArgMask.OpArgU, OpMode.iAx); // extra (larger) argument for previous opcode
 
     static bool s_Init = false;
     static List<OpCode> s_opCodeList = new List<OpCode>();
@@ -185,8 +185,9 @@ public struct OpCode
     OpArgMask argBMode; // B arg mode
     OpArgMask argCMode; // C arg mode
     OpMode opMode; // op mode
+    string name;
 
-    OpCode(int testFlag, int setAFlag,
+    OpCode(string name, int testFlag, int setAFlag,
        OpArgMask argBMode, OpArgMask argCMode, OpMode opMode)
     {
         this.testFlag = testFlag;
@@ -194,5 +195,6 @@ public struct OpCode
         this.argBMode = argBMode;
         this.argCMode = argCMode;
         this.opMode = opMode;
+        this.name = name;
     }
 }
--- selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/Program.cs
+++ selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/Program.cs
@@ -13,6 +13,8 @@ class Program
 
     static void Main(string[] args)
     {
+        OpCode opCode = OpCode.GetValue(0);
+
         byte[] data = File.ReadAllBytes(@"E:\gitHub\luaLearn\LuaLearn\originLua5.3.4\originLua5.3.4\Debug\luac.out");
         BinaryChunk chunk = ProcessLuaData.ProcessData(data);
         PrintPrototype.list(ProcessLuaData.Undump(chunk));
