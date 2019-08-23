e72ab03bf14dad547ea1d2242b0a282346dd9478
wth <690879430@qq.com>
2019-08-23 17:15:18 +0800

    添加日志

---
+++ selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/PrintStackData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class PrintStackData
+{
+    public static void printStack(LuaState ls)
+    {
+        int top = ls.getTop();
+        for (int i = 1; i <= top; i++)
+        {
+            LuaType t = ls.type(i);
+            switch (t)
+            {
+                case LuaType.LUA_TBOOLEAN:
+                    Console.Write("[{0}]", ls.toBoolean(i));
+                    break;
+                case LuaType.LUA_TNUMBER:
+                    if (ls.isInteger(i))
+                    {
+                        Console.Write("[{0}]", ls.toInteger(i));
+                    }
+                    else
+                    {
+                        Console.Write("[{0}]", ls.toNumber(i));
+                    }
+                    break;
+                case LuaType.LUA_TSTRING:
+                    Console.Write("[\"{0}\"]", ls.toString(i));
+                    break;
+                default: // other values
+                    Console.Write("[{0}]", ls.typeName(t));
+                    break;
+            }
+        }
+        //System.out.println();
+        Console.WriteLine();
+    }
+}
+
--- selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/Program.cs
+++ selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/Program.cs
@@ -15,10 +15,31 @@ class Program
     {
         //OpCode opCode = OpCode.GetValue(0);
 
-        //byte[] data = File.ReadAllBytes(@"E:\gitHub\luaLearn\LuaLearn\originLua5.3.4\originLua5.3.4\Debug\luac.out");
-        byte[] data = File.ReadAllBytes(@"E:\Dev\GitHub_Self\luaLearn\LuaLearn\originLua5.3.4\originLua5.3.4\Debug\luac.out");
+        byte[] data = File.ReadAllBytes(@"E:\gitHub\luaLearn\LuaLearn\originLua5.3.4\originLua5.3.4\Debug\luac.out");
+        //byte[] data = File.ReadAllBytes(@"E:\Dev\GitHub_Self\luaLearn\LuaLearn\originLua5.3.4\originLua5.3.4\Debug\luac.out");
         BinaryChunk chunk = ProcessLuaData.ProcessData(data);
         PrintPrototype.list(ProcessLuaData.Undump(chunk));
+
+        LuaState ls = new LuaStateImpl();
+
+        ls.pushBoolean(true);
+        PrintStackData.printStack(ls);
+        ls.pushInteger(10);
+        PrintStackData.printStack(ls);
+        ls.pushNil();
+        PrintStackData.printStack(ls);
+        ls.pushString("hello");
+        PrintStackData.printStack(ls);
+        ls.pushValue(-4);
+        PrintStackData.printStack(ls);
+        ls.replace(3);
+        PrintStackData.printStack(ls);
+        ls.setTop(6);
+        PrintStackData.printStack(ls);
+        ls.remove(-3);
+        PrintStackData.printStack(ls);
+        ls.setTop(-5);
+        PrintStackData.printStack(ls);
 
         Console.WriteLine("selflua end");
         Console.ReadLine();
