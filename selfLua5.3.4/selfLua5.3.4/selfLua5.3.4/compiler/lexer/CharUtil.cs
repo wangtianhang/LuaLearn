b8f8f25d853b99998bba7e70647328bbd5fd8ad2
wth <690879430@qq.com>
2019-08-26 14:58:56 +0800

    部分词法分析代码

---
+++ selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/compiler/lexer/CharUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CharUtil
+{
+    static bool isWhiteSpace(char c)
+    {
+        switch (c)
+        {
+            case '\t':
+            case '\n':
+            //case 0x0B: // \v
+            case '\v':
+            case '\f':
+            case '\r':
+            case ' ':
+                return true;
+        }
+        return false;
+    }
+
+    static bool isNewLine(char c)
+    {
+        return c == '\r' || c == '\n';
+    }
+
+    static bool isDigit(char c)
+    {
+        return c >= '0' && c <= '9';
+    }
+
+    static bool isLetter(char c)
+    {
+        return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+    }
+}
---
+++ selfLua5.3.4/selfLua5.3.4/selfLua5.3.4/compiler/lexer/Token.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Token
+{
+    static Dictionary<String, TokenKind> keywords = new Dictionary<String, TokenKind>();
+
+    static int Init()
+    {
+        keywords.Add("and", TokenKind.TOKEN_OP_AND);
+        keywords.Add("break", TokenKind.TOKEN_KW_BREAK);
+        keywords.Add("do", TokenKind.TOKEN_KW_DO);
+        keywords.Add("else", TokenKind.TOKEN_KW_ELSE);
+        keywords.Add("elseif", TokenKind.TOKEN_KW_ELSEIF);
+        keywords.Add("end", TokenKind.TOKEN_KW_END);
+        keywords.Add("false", TokenKind.TOKEN_KW_FALSE);
+        keywords.Add("for", TokenKind.TOKEN_KW_FOR);
+        keywords.Add("function", TokenKind.TOKEN_KW_FUNCTION);
+        keywords.Add("goto", TokenKind.TOKEN_KW_GOTO);
+        keywords.Add("if", TokenKind.TOKEN_KW_IF);
+        keywords.Add("in", TokenKind.TOKEN_KW_IN);
+        keywords.Add("local", TokenKind.TOKEN_KW_LOCAL);
+        keywords.Add("nil", TokenKind.TOKEN_KW_NIL);
+        keywords.Add("not", TokenKind.TOKEN_OP_NOT);
+        keywords.Add("or", TokenKind.TOKEN_OP_OR);
+        keywords.Add("repeat", TokenKind.TOKEN_KW_REPEAT);
+        keywords.Add("return", TokenKind.TOKEN_KW_RETURN);
+        keywords.Add("then", TokenKind.TOKEN_KW_THEN);
+        keywords.Add("true", TokenKind.TOKEN_KW_TRUE);
+        keywords.Add("until", TokenKind.TOKEN_KW_UNTIL);
+        keywords.Add("while", TokenKind.TOKEN_KW_WHILE);
+
+        return 0;
+    }
+
+    static int tmp = Init();
+
+    private int line;
+    private TokenKind kind;
+    private String value;
+
+    public Token(int line, TokenKind kind, String value)
+    {
+        this.line = line;
+        this.kind = kind;
+        this.value = value;
+    }
+}
+
