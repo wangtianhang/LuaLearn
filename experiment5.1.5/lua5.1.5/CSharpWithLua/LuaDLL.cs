ac4fcf7673041764f997b4861564603063692514
wth <690879430@qq.com>
2019-09-13 23:30:09 +0800

    添加csharp测试工程

---
+++ experiment5.1.5/lua5.1.5/CSharpWithLua/LuaDLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class LuaDLL
+{
+    public const string luaDLLName = "lua5.1.5.dll";
+}
+
