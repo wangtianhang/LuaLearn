cdac4f0c502da8b7e3e9797c93e123c6ba2cfe7e
wth <690879430@qq.com>
2019-09-14 22:48:55 +0800

    补提交

--- experiment5.1.5/lua5.1.5/lua5.1.5/ctest.lua
+++ experiment5.1.5/lua5.1.5/lua5.1.5/ctest.lua
@@ -1,10 +1,15 @@
 print('hello lua')
 
+meta = {}
+
 test = {}
 test.a = 1
 test.b = 2
 test.c = { d = 3 }
 test.e = function() end
+setmetatable(test, meta)
+
+print('test metatable', getmetatable(test))
 
 for k, v in pairs(test) do
 	print(tostring(k), '  ', tostring(v))
