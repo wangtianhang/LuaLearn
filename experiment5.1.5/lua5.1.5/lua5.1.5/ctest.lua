f9a5a29206dec90c3a326878a2f320242c56b740
wth <690879430@qq.com>
2019-09-14 22:45:38 +0800

    补提交

--- experiment5.1.5/lua5.1.5/lua5.1.5/ctest.lua
+++ experiment5.1.5/lua5.1.5/lua5.1.5/ctest.lua
@@ -4,8 +4,17 @@ test = {}
 test.a = 1
 test.b = 2
 test.c = { d = 3 }
+test.e = function() end
+
+for k, v in pairs(test) do
+	print(tostring(k), '  ', tostring(v))
+end
+
+print('=================')
 
 LogTable2(test)
+
+print('=================')
 
 Helper.LogTable(test)
 
